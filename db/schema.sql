ea20fe23749d6163075e3bac6458514156f547ee
teewalk32 <teewalk4321@gmail.com>
2023-05-20 13:13:07 -0500

    added to tables in schema file

--- db/schema.sql
+++ db/schema.sql
@@ -1,25 +1,41 @@
 DROP DATABASE IF EXISTS company_db;
 CREATE DATABASE company_db;
 
+
 USE company_db;
 
+
 CREATE TABLE department (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  department_name VARCHAR(30) NOT NULL
+  id INT NOT NULL AUTO_INCREMENT,
+  department_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
 );
+
 
 CREATE TABLE roles (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT,
   title VARCHAR(30) NOT NULL,
   salary DECIMAL,
-  department_id INT
+  department_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (department_id)
+  REFERENCES department(id)
+  ON DELETE SET NULL
 );
 
+
 CREATE TABLE employee (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  id INT NOT NULL AUTO_INCREMENT,
   first_name VARCHAR(30) NOT NULL,
   last_name VARCHAR(30) NOT NULL,
-  role_id VARCHAR(30) NOT NULL,
-  manager_name VARCHAR(30) NOT NULL
+  role_id INT NOT NULL,
+  manager_id INT  NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id)
+  REFERENCES roles(id)
+  ON DELETE SET NULL,
+  FOREIGN KEY (manager_id)
+  REFERENCES employee(id)
+  ON DELETE SET NULL
 );
 
